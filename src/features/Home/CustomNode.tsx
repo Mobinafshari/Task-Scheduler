7aa7c5efe40d3dd6d2bed6e06e0c71f101a7153f
Mobinafshari <mma137813781378@gmail.com>
2025-05-27 21:50:06 +0330

    feat: trying to handle task complete

--- src/features/Home/CustomNode.tsx
+++ src/features/Home/CustomNode.tsx
@@ -4,10 +4,29 @@ import { Checkbox, IconButton } from '@mui/material';
 import CustomIcon from '@assets/Icon';
 import toast from 'react-hot-toast';
 import useTopologicalSort from '@features/hook/useTopologicalSort';
+import { ChangeEvent, MouseEvent } from 'react';
 const CustomNode = ({ data, id }: NodeProps) => {
   const { setNodes } = useReactFlow();
   const { sorted } = useTopologicalSort();
-  console.log('===>>', sorted);
+  const handleDeleteNode = () => {
+    setNodes((nds) => nds.filter((n) => n.id !== id));
+    toast.loading('Task Deleted Successfully', {
+      icon: (
+        <CustomIcon icon="Info" svgProps={{ width: '40px', height: '40px' }} />
+      ),
+      duration: 1500,
+      style: {
+        backgroundColor: '#bde0fe',
+      },
+      position: 'top-left',
+    });
+  };
+  const handleCompleteTask = (e: ChangeEvent<HTMLInputElement>) => {
+    if (sorted.indexOf(id) !== -1 || sorted.indexOf(id) !== 0) {
+      e.preventDefault();
+      return;
+    }
+  };
   return (
     <div className={styles.node}>
       <div className={styles.label}>{data.label}</div>
@@ -23,26 +42,8 @@ const CustomNode = ({ data, id }: NodeProps) => {
         style={{ width: 10, height: 10 }}
       />
       <div className={styles['node__icons']}>
-        <Checkbox sx={{ padding: 0 }} />
-        <IconButton
-          sx={{ padding: 0 }}
-          onClick={() => {
-            setNodes((nds) => nds.filter((n) => n.id !== id));
-            toast.loading('Task Deleted Successfully', {
-              icon: (
-                <CustomIcon
-                  icon="Info"
-                  svgProps={{ width: '40px', height: '40px' }}
-                />
-              ),
-              duration: 1500,
-              style: {
-                backgroundColor: '#bde0fe',
-              },
-              position: 'top-left',
-            });
-          }}
-        >
+        <Checkbox sx={{ padding: 0 }} onClick={(e) => handleCompleteTask(e)} />
+        <IconButton sx={{ padding: 0 }} onClick={handleDeleteNode}>
           <CustomIcon
             svgProps={{ width: '26px', height: '26px' }}
             icon="Delete"
